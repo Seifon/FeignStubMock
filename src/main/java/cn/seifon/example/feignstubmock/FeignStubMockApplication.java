b66e4722200bc14fd800f6a86a3992fba5a01861
XiongFeng <xiongfeng@worldunion.com.cn>
2019-01-25 18:17:54 +0800

    first commit

---
+++ src/main/java/cn/seifon/example/feignstubmock/FeignStubMockApplication.java
@@ -0,0 +1,14 @@
+package cn.seifon.example.feignstubmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignStubMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignStubMockApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/cn/seifon/example/feignstubmock/FeignStubMockApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.seifon.example.feignstubmock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FeignStubMockApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
